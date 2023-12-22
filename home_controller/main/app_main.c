032dbfc8c72a3f90f346b0a846478ea6dc30a254
Oscar Vestlie <oscar.vestlie@gmail.com>
2023-12-22 15:04:12 -0800

    Start of project structure

---
+++ home_controller/main/app_main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "esp_log.h"
+#include "esp_system.h"
+
+
+static const char *TAG = "HOME_CONTROLLER_MQTT";
+
+void log_setup(void) {
+  esp_log_level_set("*", ESP_LOG_INFO);
+  esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+  esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+  esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
+  esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
+  esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+  esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+}
+
+void app_main(void) {
+  ESP_LOGI(TAG, "[APP] Startup..");
+  ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+  ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+  log_setup();
+
+  // TODO
+}
